939ae378e4d607933874682c1dcaebd1a07425eb
CyTon Code <cytoncode@gmail.com>
2021-02-28 16:46:39 +0200

    Create check_open_file

---
+++ check_open_file/README.md
@@ -0,0 +1,18 @@
+# File readability check function.
+
+
+<h4>
+ What can you expect from her:
+</h4>
+
+- "False" - The file is open (the address is correct).
+
+- "True" - The file was not opened (incorrect address).
+
+- "Exception" - the file is not closed.
+
+<h4>
+ Detailed instructions for using the module:
+</h4>
+
+- [-info checkopen](https://github.com/CyTon-Code/WorkingWithFiles/blob/main/checkopen/info.txt)
---
+++ check_open_file/main.py
@@ -0,0 +1,37 @@
+"""
+    The module works only through import.
+    Via os.system or return (RUN) - doesn't work.
+"""
+
+from plugin import file_close
+
+if __name__ == "__main__":  # If not imported, I exit is the module:
+    print("I am is Module!!! Bye Bye!!!")
+    exit()  # Answer: I'm leaving, I'm a module.
+
+
+def check_file(link: str) -> bool or Exception:  # Check if the file exists:
+    """
+        This module is required to read a file (or module).
+        It can also check if a file (or module) exists.
+    """
+    # Credo: Checking file readability...
+
+    file_open = True
+    f = None
+
+    try:  # Opens a file:
+        f = open(link, 'r')
+        return False  # Answer: The file is there.
+
+    except:  # The file did not open:
+        print("file is not defined. file:", link)
+        file_open = False  # At the end of the function, the file cannot be
+        # closed (it has not been opened).
+
+        return not file_open  # Answer: The file is missing or has not been read.
+
+    finally:
+        if file_open:  # If the file open:
+            file_close(f, "Я пытался открывать файл.")
+
