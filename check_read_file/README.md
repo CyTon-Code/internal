245f9f47aa0288bd4bb00ee420be61fd1f384f7f
CyTon Code <cytoncode@gmail.com>
2021-02-28 17:42:00 +0200

    Update All

---
+++ check_read_file/README.md
@@ -0,0 +1,13 @@
+# File readability check function.
+
+###### What can you expect from her:
+
+- 'False' - File read (address is correct).
+
+- 'True' - The file did not open (the address is incorrect).
+
+- 'Exception' or 'None' - The file did not close or was not read.
+
+###### Detailed instructions for using the module:
+
+- [-info check_read_file](info.txt)
--- check_read_file/main.py
+++ check_read_file/main.py
@@ -4,13 +4,14 @@
 """
 
 from additional.file_close import file_close
+from additional.file_read import file_read
 
 if __name__ == "__main__":  # If not imported, I exit is the module:
     print("I am is Module!!! Bye Bye!!!")
     exit()  # Answer: I'm leaving, I'm a module.
 
 
-def check_file(link: str) -> bool or Exception:  # Check if the file exists:
+def check_read_file(link: str) -> bool or Exception or None:  # Check if the file exists:
     """
         This module is required to read a file (or module).
         It can also check if a file (or module) exists.
@@ -18,19 +19,20 @@ def check_file(link: str) -> bool or Exception:  # Check if the file exists:
     # Credo: Checking file readability...
 
     file_open = True
-    f = None
+    file = None
 
     try:  # Opens a file:
-        f = open(link, 'r')
-        return False  # Answer: The file is there.
+        file = open(link, 'r')
+        return file_read(file)
 
     except:  # The file did not open:
-        print("file is not defined. file:", link)
+        print("File is not defined. File:", link)
         file_open = False  # At the end of the function, the file cannot be
         # closed (it has not been opened).
 
-        return not file_open  # Answer: The file is missing or has not been read.
+        return not file_open  # Answer: The file is missing or has not been
+        # read.
 
     finally:
         if file_open:  # If the file open:
-            file_close(f, "Я пытался открывать файл.")
+            file_close(file, "Я пытался открывать и читать файл.")
